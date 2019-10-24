9893ffc415f286436bfabd2467d2522b96e82458
Michele Bonazza <michele@sketchtogether.com>
2019-10-24 21:56:39 +0200

    Remove useless models

--- src/models/vehicles.ts
+++ src/models/vehicles.ts
@@ -1,5 +1,3 @@
-import { ImageRequireSource } from 'react-native';
-
 export interface Coordinates {
   lat: number;
   lon: number;
@@ -7,15 +5,6 @@ export interface Coordinates {
 
 export interface Location extends Coordinates {
   address?: string;
-}
-
-export interface BoundingBox {
-  topLeft: Coordinates;
-  bottomRight: Coordinates;
-}
-
-export interface BoundingBoxWithId extends BoundingBox {
-  id: string;
 }
 
 export type PinType =
@@ -28,66 +17,3 @@ export type PinType =
   | 'scooter'
   | 'station'
   | 'taxi';
-
-const vehicleTypes = ['bike', 'car', 'kickscooter', 'scooter'];
-
-export const isVehicle = (arg: { type: PinType }): arg is Vehicle =>
-  vehicleTypes.indexOf(arg.type) > -1;
-
-export interface Pin {
-  key: string;
-  id: string;
-  image: ImageRequireSource;
-  location: Location;
-  provider: string;
-  type: PinType;
-  name?: string;
-  filteredOut?: boolean;
-  cacheUntil?: number;
-}
-
-export interface Vehicle extends Pin {
-  licensePlate?: string;
-  model?: string;
-  transmission?: 'manual' | 'automatic';
-  fuel?: Fuel;
-  seats?: number;
-  babySeat?: boolean;
-  prices?: Prices;
-  priceId?: string;
-  cacheUntil?: number;
-  imageUrl?: string;
-}
-
-export interface Bike extends Vehicle {
-  electric?: true;
-}
-
-export interface Station extends Pin {
-  vehicleCount: number;
-  ebikes?: number;
-  parkingSlots?: number;
-}
-
-export interface Fuel {
-  type: 'diesel' | 'electric' | 'hybrid' | 'hydrogen' | 'petrol';
-  percent?: number;
-  range?: number;
-}
-
-export interface Price {
-  amount: number;
-  unit: 'min' | 'hour' | 'day' | 'km' | 'custom';
-  fixed?: number;
-  offer?: string;
-  label?: string;
-}
-
-export interface Prices {
-  currency: string;
-  moving?: Price[];
-  parked?: Price[];
-  daily?: Price[];
-  offers?: Price[];
-  label?: string;
-}
