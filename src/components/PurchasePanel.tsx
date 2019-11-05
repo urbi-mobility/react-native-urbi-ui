93b4b4809f55a6d7aaf4581da635ce21ed6a0b34
domenicogabriele <dommy.g1985@gmail.com>
2019-11-05 09:48:12 +0100

    Change heightPurchasePanel => purchasePanelHeight

--- src/components/PurchasePanel.tsx
+++ src/components/PurchasePanel.tsx
@@ -4,11 +4,11 @@ import { ButtonCompact } from '../molecules/buttons/ButtonCompact';
 import { PurchaseProps, PurchaseUnmemoized } from '../molecules/content/Purchase';
 import { colors } from '../utils/colors';
 
-export const heightPurchasePanel = 92;
+export const purchasePanelHeight = 92;
 const styles = StyleSheet.create({
   Wrapper: {
     flex: 1,
-    height: heightPurchasePanel,
+    height: purchasePanelHeight,
     backgroundColor: colors.ulisse,
     borderTopRightRadius: 15,
     borderTopLeftRadius: 15,
