4698070771c1837b1587faf7fe66f8a977010feb
domenicogabriele <dommy.g1985@gmail.com>
2019-11-29 16:02:20 +0100

    Add shadow and animation to PaymentPanel

--- showcase/screens/PaymentPanel.tsx
+++ showcase/screens/PaymentPanel.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
 import { PaymentPanel as PaymentPanelComponent } from 'react-native-urbi-ui/components/PaymentPanel';
 import { ButtonRegular } from 'react-native-urbi-ui/molecules/buttons/ButtonRegular';
-import { onButtonPress, placeholder, renderComponent } from '../utils/ComponentPreview';
+import { onButtonPress, placeholder, renderComponent, sleep } from '../utils/ComponentPreview';
 
 type PaymentPanelState = {
   showPanel: boolean;
+  progress: boolean;
 };
 
 class PaymentPanel extends React.PureComponent<any, PaymentPanelState> {
   constructor(props: any) {
     super(props);
-    this.state = { showPanel: false };
+    this.state = { showPanel: false, progress: false };
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
   }
@@ -24,18 +25,27 @@ class PaymentPanel extends React.PureComponent<any, PaymentPanelState> {
     this.setState({ showPanel: true });
   }
 
+  async simulateCall() {
+    this.setState({ progress: true });
+    await sleep(1000);
+    this.setState({ progress: false });
+  }
+
   render() {
     return (
       <>
         <PaymentPanelComponent
           show={this.state.showPanel}
+          loading={this.state.progress}
           cancel="cancel"
           onCancelPress={this.hideModal}
           paymentTitle="Payment method"
           title="YOUR ORDER"
           nameShop="Item name goes here"
           buttonTitle="CONFIRM AND PAY"
-          buttonOnPress={onButtonPress}
+          buttonOnPress={() => {
+            this.simulateCall();
+          }}
           price="20.90€"
           cardProperty={{
             haveCard: true,
--- src/components/PaymentPanel.tsx
+++ src/components/PaymentPanel.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
-import { ImageRequireSource, StyleSheet, Text, TextStyle, View, ViewStyle } from 'react-native';
+import React, { useLayoutEffect, useRef } from 'react';
+import {
+  ActivityIndicator,
+  ImageRequireSource,
+  StyleSheet,
+  Text,
+  TextStyle,
+  View,
+  ViewStyle,
+} from 'react-native';
+import { Transition, Transitioning, TransitioningView } from 'react-native-reanimated';
+import { ButtonRegular } from '../molecules/buttons/ButtonRegular';
 import { LinkCompact } from '../molecules/buttons/LinkCompact';
+import { IconAndLabel } from '../molecules/content/IconAndLabel';
 import { SectionsDivider } from '../molecules/SectionsDivider';
 import { colors } from '../utils/colors';
 import { textStyle as makeTextStyle } from '../utils/textStyles';
 import { ListItemLarge } from './ListItemLarge';
-import { IconAndLabel } from '../molecules/content/IconAndLabel';
-import { ButtonRegular } from '../molecules/buttons/ButtonRegular';
 
 const styles = StyleSheet.create({
   Wrapper: {
     position: 'absolute',
+    top: 0,
     right: 0,
     bottom: 0,
     left: 0,
-    backgroundColor: colors.ulisse,
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
     zIndex: 666,
+  } as ViewStyle,
+  WrapperChild: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    left: 0,
+    borderTopRightRadius: 15,
+    borderTopLeftRadius: 15,
+    shadowOffset: { height: 4, width: 0 },
+    backgroundColor: colors.ulisse,
   } as ViewStyle,
   Cancel: {
     flex: 1,
@@ -34,6 +54,13 @@ const styles = StyleSheet.create({
     marginRight: 12,
     marginBottom: 12,
   } as TextStyle,
+  ContainerCenter: {
+    paddingTop: 150,
+    paddingBottom: 150,
+    marginLeft: 20,
+    marginRight: 20,
+    alignItems: 'center',
+  } as ViewStyle,
   Footer: {
     flex: 1,
     alignItems: 'center',
@@ -41,14 +68,17 @@ const styles = StyleSheet.create({
     marginBottom: 24,
   } as ViewStyle,
 });
+
 type CardProperty = {
   haveCard: boolean;
   labelCard: string;
   imageCard?: ImageRequireSource;
   onCardPress?: () => any;
 };
+
 type PaymentPanelProps = {
   show: boolean;
+  loading: boolean;
   onCancelPress: () => any;
   title: string;
   nameShop: string;
@@ -60,40 +90,76 @@ type PaymentPanelProps = {
   buttonOnPress: () => any;
 };
 
-export const PaymentPanelUnmemoized = (props: PaymentPanelProps) =>
-  props.show ? (
-    <View style={styles.Wrapper} elevation={5}>
-      <View style={styles.Cancel}>
-        <LinkCompact text={props.cancel} onPress={props.onCancelPress} uppercase />
-      </View>
-      <SectionsDivider label={props.title} />
-      <Text style={styles.TitleOrder}>{props.nameShop}</Text>
-      <Text style={styles.PriceOrder}>{props.price}</Text>
-      <View style={{ flex: 1 }}>
-        <SectionsDivider label={props.paymentTitle} />
-        <ListItemLarge
-          onPress={props.cardProperty.onCardPress}
-          content={
-            <IconAndLabel
-              image={props.cardProperty.imageCard}
-              label={props.cardProperty.labelCard}
-              labelColor={colors.uma}
-            />
-          }
-          icon="disclosure-small"
-          backgroundColor={colors.ukko}
-        />
-      </View>
-      <View style={styles.Footer}>
-        <ButtonRegular
-          buttonStyle="primary"
-          label={props.buttonTitle}
-          onPress={props.buttonOnPress}
-        />
-      </View>
-    </View>
-  ) : (
-    undefined
+const transition = (
+  <Transition.In type="slide-bottom" durationMs={150} interpolation="easeIn" propagation="bottom" />
+);
+
+export const PaymentPanelUnmemoized = (props: PaymentPanelProps) => {
+  const ref = useRef<TransitioningView>();
+
+  useLayoutEffect(() => {
+    if (ref.current) {
+      ref.current!.animateNextTransition();
+    }
+  }, [props.show]);
+
+  return (
+    <Transitioning.View
+      ref={ref}
+      transition={transition}
+      style={props.show ? styles.Wrapper : { height: 0 }}
+      elevation={5}
+    >
+      {props.show && (
+        <View style={styles.WrapperChild}>
+          {props.loading ? (
+            <View style={styles.ContainerCenter}>
+              <ActivityIndicator size="large" color={colors.brand} />
+            </View>
+          ) : (
+            <View>
+              <View style={styles.Cancel}>
+                <LinkCompact text={props.cancel} onPress={props.onCancelPress} uppercase />
+              </View>
+              <SectionsDivider
+                label={props.title}
+                labelColor={colors.ughina}
+                backgroundColor={colors.ulisse}
+              />
+              <Text style={styles.TitleOrder}>{props.nameShop}</Text>
+              <Text style={styles.PriceOrder}>{props.price}</Text>
+              <View style={{ flex: 1 }}>
+                <SectionsDivider
+                  label={props.paymentTitle}
+                  labelColor={colors.ughina}
+                  backgroundColor={colors.ulisse}
+                />
+                <ListItemLarge
+                  onPress={props.cardProperty.onCardPress}
+                  content={
+                    <IconAndLabel
+                      image={props.cardProperty.imageCard}
+                      label={props.cardProperty.labelCard}
+                      labelColor={colors.uma}
+                    />
+                  }
+                  icon="disclosure-small"
+                  backgroundColor={colors.ukko}
+                />
+              </View>
+              <View style={styles.Footer}>
+                <ButtonRegular
+                  buttonStyle="primary"
+                  label={props.buttonTitle}
+                  onPress={props.buttonOnPress}
+                />
+              </View>
+            </View>
+          )}
+        </View>
+      )}
+    </Transitioning.View>
   );
+};
 
 export const PaymentPanel = React.memo(PaymentPanelUnmemoized);
