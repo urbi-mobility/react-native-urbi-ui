57428f01ffbf468a8e87a2a3f5d7ee8a24381d6c
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-10-13 10:01:21 +0200

    moving logic to ListItemComparator
    https://github.com/urbi-mobility/react-native-urbi-ui/pull/17#discussion_r503195100

--- src/components/ListItemComparator.tsx
+++ src/components/ListItemComparator.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { ListItemProps, SeparatorStyle } from '../components/ListItem';
+import { ListItemProps, maybeAddSeparator } from '../components/ListItem';
 import { ListItemLargeStyles } from '../components/ListItemLarge';
+import { MaybeTouchable } from '../components/MaybeTouchable';
 
 const styles = StyleSheet.create(ListItemLargeStyles);
 
-const ListItemComparatorUnmemoized = (props: ListItemProps) => (
-  <>
+const renderListItem = (props: ListItemProps) => (
+  <MaybeTouchable onPress={props.onPress} backgroundColor={props.backgroundColor} exactHeight={96}>
     <View style={{ ...styles.ListItemWrapper, height: 96 }}>
       {props.content}
       {props.end}
     </View>
-    <View style={SeparatorStyle} />
-  </>
+  </MaybeTouchable>
 );
+
+export const ListItemComparatorUnmemoized = (props: ListItemProps) =>
+  maybeAddSeparator(props, renderListItem);
 
 export const ListItemComparator = React.memo(ListItemComparatorUnmemoized);
